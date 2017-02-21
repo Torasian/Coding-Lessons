e138604551fa49ca09ea998e606dc3fbdc45f605
Torasian <williamhawken@hotmail.co.uk>
2017-02-21 15:49:22 +0000

    Added some explanations and exercises for the first lesson

--- src/learning_java/Hello.java
+++ src/learning_java/Hello.java
@@ -3,8 +3,28 @@ package learning_java;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("hello world");
+	
+		//Exercise 1: Print out your name
+		
+		//A variable stores the value of a data item. This value can change.
+		// Below we create an integer variable called 'number'. We have not assigned it a value.
+		// This is called, declaring a variable.
+		int number;
+		// Here we assign number a value
+		number = 1;
+		
+		//We can declare and assign a varible like this:
+		String hello = "Hello";
+		
+		//so far we have introduced 2 data types, an integer and a String.
+		//There are many more different data types: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+		
+		//Exercise 2: Create a variable of type String and assign it to your names
+		
+		
+		
 
 	}
 
